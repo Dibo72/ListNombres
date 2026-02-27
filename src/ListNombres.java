a31994116e279ea2eb366756cd24dd3624b40f58
Dibo72 <humanida133@gmail.com>
2026-02-27 09:35:32 +0100

    Ejercicio acabado

--- src/ListNombres.java
+++ src/ListNombres.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class ListNombres {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        List<String>nombres = new ArrayList<>();
+        boolean seguir = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        do{
+            System.out.println("Introduce un nombre:");
+            String nombre = sc.nextLine();
+            if(nombre.equalsIgnoreCase("fin")){
+                seguir = false;
+            }else {
+                nombres.add(nombre);
+            }
+        }while(seguir);
+
+        for(int i=0; i<nombres.size(); i++){
+            System.out.println((i+1) + " " +  nombres.get(i));
         }
     }
 }
